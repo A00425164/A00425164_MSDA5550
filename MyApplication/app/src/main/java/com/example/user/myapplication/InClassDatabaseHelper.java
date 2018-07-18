e50ac1f1090eda59dea559ec2176fa3b53c6772c
wbomar16@gmail.com <wbomar16@gmail.com>
2018-07-18 17:21:00 -0300

    update InClass as of Monday 16/7/18

---
+++ MyApplication/app/src/main/java/com/example/user/myapplication/InClassDatabaseHelper.java
@@ -0,0 +1,45 @@
+package com.example.user.myapplication;
+
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+
+import java.util.Date;
+
+
+class InClassDatabaseHelper extends SQLiteOpenHelper{
+    public static final String DB_NAME = "in-class"; //name of the DB
+    public static final int DB_VERSION = 1; //version of the DB
+    public static final String TABLE_NAME = "PERSON"; //name of the Table
+
+    public InClassDatabaseHelper(Context context){
+        super(context,DB_NAME,null,DB_VERSION);//null is for cursors
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE" + TABLE_NAME + " ("
+        + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + "NAME TEXT, "
+        + "PASSWORD TEXT, " //Never store passwords in clear text in real apps
+        + "HEALTH_CARD_NUMB TEXT, "
+        + "DATE INTEGER);");
+
+        Date today = new Date(); //we want to start with some initial data
+        ContentValues personValues = new ContentValues();
+        personValues.put("NAME","Dan Penny");
+        personValues.put("PASSWORD","Super Secret");
+        personValues.put("HEALTH_CARD_NUMB","1234 5678 9101");
+        personValues.put("DATE",today.getTime());
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+}
